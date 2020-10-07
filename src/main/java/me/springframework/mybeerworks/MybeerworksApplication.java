682fe126d3fb286b32d04fc07736ca4bfee2a8e5
srive <srive@DESKTOP-O3BL95U>
2020-10-07 09:13:28 +0530

    Initial Commit

---
+++ src/main/java/me/springframework/mybeerworks/MybeerworksApplication.java
@@ -0,0 +1,13 @@
+package me.springframework.mybeerworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybeerworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybeerworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/springframework/mybeerworks/MybeerworksApplicationTests.java
@@ -0,0 +1,13 @@
+package me.springframework.mybeerworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybeerworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
